d25d2ab30bb3089a244b8a5cdae653b3c0957698
Jamon Holmgren <jamon@infinite.red>
2017-05-06 14:24:14 -0700

    Initial

---
+++ plugin.js
@@ -0,0 +1,8 @@
+// Ignite CLI plugin for BossBoilerplate
+// ----------------------------------------------------------------------------
+
+const add = async function (context) {}
+const remove = async function (context) {}
+
+module.exports = { add, remove }
+
